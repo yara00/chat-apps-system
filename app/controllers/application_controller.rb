e25ff6ca0bf7848f77999b83e9c51c46c6271b98
yara00 <yarahossam222@gmail.com>
2024-05-06 01:41:57 +0300

    rmv comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+    include ExceptionHandler
     include SettersMethods
     include ParamsHelper
     include ResponseHelper
-    include ExceptionHandler
 end
--- app/helpers/setters_methods.rb
+++ app/helpers/setters_methods.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 module SettersMethods
-
     def set_application
       @application = Application.find_by!(token: params[:application_token])
     end
 
     def set_chat
       @chat = Chat.find_by!(application_token: params[:application_token], number: params[:chat_number])
-     # raise
     end
   
     def set_message
